3c9d861ac0eb1cc6fcbffc557f54c633303a8219
Mayur2801 <100698001+Mayur2801@users.noreply.github.com>
2025-06-20 15:23:07 +0530

    Create About.js

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About This App</h2>
+      <p>
+        This React app is built with Create React App, Dockerized, and served as static assets via Nginx.
+      </p>
+    </div>
+  );
+}
+
+export default About;
